160392e25508dbe4e7f8cf19df8169a520a1a1a5
mvcbox <mvcbox.org@gmail.com>
2018-09-16 13:01:45 +0300

    Update to 0.9.5

--- handlers/frequency-control-unknown.js
+++ handlers/frequency-control-unknown.js
@@ -15,10 +15,7 @@ module.exports = function (options) {
 
     let counter = {};
     let banlist = {};
-
-    setInterval(function () {
-        counter = {};
-    }, options.time);
+    let resetInterval = 0;
 
     return {
         /**
@@ -40,6 +37,12 @@ module.exports = function (options) {
         handler: function (packet, input, output, next) {
             packet.payload.setPointer(0);
             let roleIdentifier = packet.opcode + '_' + packet.payload.toString('hex');
+            let interval = Date.now() / options.time;
+
+            if (interval - resetInterval > 1) {
+                resetInterval = interval;
+                counter = {};
+            }
 
             if (banlist[roleIdentifier]) {
                 return next(1);
@@ -61,79 +64,6 @@ module.exports = function (options) {
                     delete banlist[roleIdentifier];
                 }, options.bantime);
                 return next(1);
-            }
-
-            next();
-        }
-    };
-};
-
-
-module.exports = function (options) {
-    options = Object.assign({}, {
-        defaultFrequency: {
-            count: 50,
-            time: 5000
-        }
-    }, options || {});
-
-    let banlist = {};
-    let storage = {};
-
-    setInterval(function () {
-        storage = {};
-    }, 60000);
-
-    return {
-        /**
-         * @type {Array}
-         */
-        only: undefined,
-
-        /**
-         * @type {Array}
-         */
-        except: [0x4B],
-
-        /**
-         * @param {Object} packet
-         * @param {Socket} input
-         * @param {Socket} output
-         * @param {Function} next
-         */
-        handler: function (packet, input, output, next) {
-            packet.payload.setPointer(0);
-            let frequency = options.defaultFrequency;
-            let storageKey = packet.opcode + '_' + packet.payload.toString('hex');
-
-            packet.payload.setPointer(0);
-
-            if (banlist[storageKey]) {
-                return next(1);
-            }
-
-            if (!storage[storageKey]) {
-                storage[storageKey] = {
-                    tstamp: Date.now(),
-                    count: 1
-                };
-
-                return next();
-            }
-
-            if (++storage[storageKey].count > frequency.count) {
-                if (Date.now() - storage[storageKey].tstamp < frequency.time) {
-                    console.log("\n[" + new Date().toLocaleString() + ']: ============ Frequency control unknown ============');
-                    console.log('Opcode:', '0x' + packet.opcode.toString(16).toUpperCase());
-                    console.log('Content:', storageKey);
-                    banlist[storageKey] = true;
-                    return next(1);
-                }
-
-                storage[storageKey] = {
-                    tstamp: Date.now(),
-                    count: 1
-                };
             }
 
             next();
