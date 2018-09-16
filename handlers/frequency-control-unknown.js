2e4f8944f10c8fbdea28d600c8e1d0ea4d51e902
mvcbox <mvcbox.org@gmail.com>
2018-09-16 10:46:58 +0300

    Update to 0.9.4

--- handlers/frequency-control-unknown.js
+++ handlers/frequency-control-unknown.js
@@ -1,10 +1,79 @@
 'use strict';
 
+/**
+ * @param {Object} options
+ * @returns {{only: number[], except: undefined, handler: handler}}
+ */
+module.exports = function (options) {
+    console.log('============= Frequency control unknown - init =============');
+
+    options = Object.assign({
+        time: 3000, //ms
+        count: 50,
+        bantime: 600000,
+    }, options || {});
+
+    let counter = {};
+    let banlist = {};
+
+    setInterval(function () {
+        counter = {};
+    }, options.time);
+
+    return {
+        /**
+         *
+         */
+        only: undefined,
+
+        /**
+         *
+         */
+        except: [0x4B, 0x82],
+
+        /**
+         * @param {Object} packet
+         * @param {Socket} input
+         * @param {Socket} output
+         * @param {Function} next
+         */
+        handler: function (packet, input, output, next) {
+            packet.payload.setPointer(0);
+            let roleIdentifier = packet.opcode + '_' + packet.payload.toString('hex');
+
+            if (banlist[roleIdentifier]) {
+                return next(1);
+            }
+
+            if (!counter[roleIdentifier]) {
+                counter[roleIdentifier] = 1;
+                return next();
+            }
+
+            if (++counter[roleIdentifier] > options.count) {
+                console.log('=========================================================================');
+                console.log(`[${new Date().toLocaleString()}]: Frequency control unknown - block`);
+                console.log('RoleIdentifier:', roleIdentifier);
+                console.log('Packet opcode:', '0x' + packet.opcode.toString(16).toUpperCase());
+                console.log('=========================================================================');
+                banlist[roleIdentifier] = true;
+                setTimeout(function () {
+                    delete banlist[roleIdentifier];
+                }, options.bantime);
+                return next(1);
+            }
+
+            next();
+        }
+    };
+};
+
+
 module.exports = function (options) {
     options = Object.assign({}, {
         defaultFrequency: {
-            count: 25,
-            time: 1000
+            count: 50,
+            time: 5000
         }
     }, options || {});
 
@@ -24,7 +93,7 @@ module.exports = function (options) {
         /**
          * @type {Array}
          */
-        except: undefined,
+        except: [0x4B],
 
         /**
          * @param {Object} packet
